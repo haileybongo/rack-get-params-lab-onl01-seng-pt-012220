ce3f92cabce0e27e083cb0c2995cc7f8eb571946
Hailey Bongo <haileyebongo@gmail.com>
2020-04-13 17:26:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,13 +11,16 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      
     elsif req.path.match(/cart/)
       @@cart.each.do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/add/)
       add_item = req.params.["item"]
       
@@ -30,7 +33,7 @@ class Application
     else
       resp.write "Path Not Found"
     end
-  end
+
     resp.finish
   end
 
