5971e37d5325dd4c25ce92d0a047204b33f4b3e7
Hailey Bongo <haileyebongo@gmail.com>
2020-04-13 17:24:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,7 @@ class Application
     elsif req.path.match(/cart/)
       @@cart.each.do |item|
         resp.write "#{item}\n"
+      end
     elsif req.path.match(/add/)
       add_item = req.params.["item"]
       
